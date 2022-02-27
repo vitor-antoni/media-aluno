a43c0e823db933b8a90c99127a34392bc22b3fb0
Vitor Antoni <v.silva.antoni@gmail.com>
2022-02-27 20:57:17 -0300

    Update for improve the code

--- Media-aluno.py
+++ Media-aluno.py
@@ -4,8 +4,12 @@ while True:
     aluno = str(input('Digite o nome do aluno: '))
     turma.append([f'{aluno}'])
     nota_1 = float(input('Digite sua primeira nota: '))
+    while nota_1 > 10:
+        nota_1 = float(input('Nota digitada inválida! Sua nota deve estar entre 0 e 10. Tente novamente: '))
     turma[cont_aluno].append(nota_1)    # Adiciona na lista, a nota do aluno de acordo com a contagem
     nota_2 = float(input('Digite sua segunda nota: '))
+    while nota_2 > 10:
+        nota_2 = float(input('Nota digitada inválida! Sua nota deve estar entre 0 e 10. Tente novamente: '))
     turma[cont_aluno].append(nota_2)
     cont_aluno += 1
     continuar = str(input('Deseja adicionar outra pessoa [S/N]? ')).upper().strip()[0]
@@ -21,14 +25,12 @@ for c in turma:
     escolha = int(input('Qual pessoa você deseja saber a média? '))
     media = (turma[escolha][1] + turma[escolha][2]) / 2     
     print(f'A média de {turma[escolha][0]} é {media}')     # Turma == lista / Escoha == sublista / 0 == nome
-    len(turma)
-    if len(turma)-1 == escolha:     # Realizará uma contagem de sublistas em TURMA e se for == a escolha
-        print('A pessoa selecionada é a última da fila. Portanto, encerrarei o programa!')
-        break
-    else:
-        mais = str(input('Deseja saber a nota de mais alguém [S/N]? ')).upper().strip()[0]
+    mais = str(input('Deseja saber a nota de mais alguém [S/N]? ')).upper().strip()[0]
     while mais not in 'SN':
-        str(input('Dígito inválido! Deseja saber a nota de mais alguém [S/N]? ')).upper().strip()[0]
+        mais = str(input('Dígito inválido! Deseja saber a nota de mais alguém [S/N]? ')).upper().strip()[0]
     if mais == 'N':
         break
-print('\nAcabou! :D')
+if mais == 'N':
+    print('\nAs notas solicitadas foram visualizadas!')
+else:
+    print('\nForam visualizadas as notas de todos os alunos!')
